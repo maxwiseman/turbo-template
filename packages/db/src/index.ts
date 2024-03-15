38c29a16edf8e07fa5c52fca99ea05c3b4ce60af
maxwiseman <max@maxwiseman.io>
2024-03-15 19:11:19 -0400

    fix(db): fixed incorrect db environment variables

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -11,10 +11,8 @@ export { mySqlTable as tableCreator } from "./schema/_table";
 export * from "drizzle-orm";
 
 const client = createClient({
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- the env is validated in the build step
-  url: process.env.DATABASE_URL!,
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- the env is validated in the build step
-  authToken: process.env.DATABASE_AUTH_TOKEN!,
+  url: process.env.DB_URL ?? "",
+  authToken: process.env.DB_AUTH_TOKEN ?? "",
 });
 
 export const db = drizzle(client, { schema });
